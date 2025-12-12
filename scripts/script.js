96655423c5a15a8ba00178fec8f36e5a2a99fcb3
bergsonall <bruninhodubal@hotmail.com>
2025-12-12 10:32:52 -0300

    add ":active" with scroll mobile

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,17 @@ document.addEventListener("DOMContentLoaded", () => {
         document.body.classList.toggle("dark");
     };
 });
+
+const elementos = document.querySelectorAll('.course')
+
+    window.addEventListener('scroll', () => {
+        elementos.forEach(el => {
+            const rect = el.getBoundingClientRect();
+
+            if (rect.top <= 200 && rect.bottom >= 0) {
+                el.classList.add('active');
+            } else {
+                el.classList.remove('active');
+            }
+        })
+})
