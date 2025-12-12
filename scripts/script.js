57be43eb19f7a8204a4856c3f893f6540da32b7b
bergsonall <bruninhodubal@hotmail.com>
2025-12-12 10:38:27 -0300

    att js

--- scripts/script.js
+++ scripts/script.js
@@ -11,16 +11,16 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 });
 
-const elementos = document.querySelectorAll('.course')
+const elementos = document.querySelectorAll('.box');
 
-    window.addEventListener('scroll', () => {
-        elementos.forEach(el => {
-            const rect = el.getBoundingClientRect();
+window.addEventListener('scroll', () => {
+  elementos.forEach(el => {
+    const rect = el.getBoundingClientRect();
 
-            if (rect.top <= 200 && rect.bottom >= 0) {
-                el.classList.add('active');
-            } else {
-                el.classList.remove('active');
-            }
-        })
-})
+    if (rect.top <= 200 && rect.bottom >= 0) {
+      el.classList.add('active');
+    } else {
+      el.classList.remove('active');
+    }
+  });
+});
